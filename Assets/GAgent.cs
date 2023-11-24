8010359bf2dd2a26e070b8ffcde822e117d8b4c6
jofto <slammerex03@gmail.com>
2023-11-24 19:29:50 +0000

    1. Executing a Simple Plan

--- Assets/GAgent.cs
+++ Assets/GAgent.cs
@@ -3,12 +3,9 @@ using UnityEngine;
 
 public class SubGoal {
 
-    // Dictionary to store our goals
     public Dictionary<string, int> sGoals;
-    // Bool to store if goal should be removed
     public bool remove;
 
-    // Constructor
     public SubGoal(string s, int i, bool r) {
 
         sGoals = new Dictionary<string, int>();
@@ -21,14 +18,12 @@ public class GAgent : MonoBehaviour {
 
     public List<GAction> actions = new List<GAction>();
     public Dictionary<SubGoal, int> goals = new Dictionary<SubGoal, int>();
-    public WorldStates beliefs = new WorldStates();
 
     GPlanner planner;
     Queue<GAction> actionQueue;
-    public GAction currentAvtion;
+    public GAction currentAction;
     SubGoal currentGoal;
 
-    // Start is called before the first frame update
     void Start() {
 
         GAction[] acts = this.GetComponents<GAction>();
@@ -38,10 +33,73 @@ public class GAgent : MonoBehaviour {
         }
     }
 
-    // Update is called once per frame
+    bool invoked = false;
+    void CompleteAction()
+    {
+        currentAction.running = false;
+        currentAction.PostPerform();
+        invoked = false;
+    }
+
     void LateUpdate() {
 
+        if (currentAction != null && currentAction.running)
+        {
+            if (currentAction.agent.hasPath && currentAction.agent.remainingDistance < 1f)
+            {
+                if (!invoked)
+                {
+                    Invoke("CompleteAction", currentAction.duration);
+                    invoked = true;
+                }
+            }
+            return;
+        }
+
+        if (planner == null || actionQueue == null)
+        {
+            planner = new GPlanner();
+
+            var sortedGoals = from entry in goals orderby entry.Value descending select entry;
+
+            foreach (KeyValuePair<SubGoal, int> sg in sortedGoals)
+            {
+                actionQueue = planner.plan(actions, sg.Key.sgoals, null);
+                if(actionQueue != null)
+                {
+                    currentGoal = sg.Key;
+                    break;
+                }
+            }
+        }
+
+        if (actionQueue != null && actionQueue.Count == 0)
+        {
+            if (currentGoal.remove)
+            {
+                goals.Remove(currentGoal);
+            }
+            planner = null;
+        }
+
+        if (actionQueue != null && actionQueue.Count > 0)
+        {
+            currentAction = actionQueue.Dequeue();
+            if (currentAction.PrePerform())
+            {
+                if(currentAction.target == null && currentAction.targetTag != "")
+                    currentAction.target = GameObject.FindWithTag(currentAction.targetTag);
+
+                if (currentAction.targetTag != null)
+                {
+                    currentAction.running = true;
+                    currentAction.agent.SetDestination(currentAction.target.transform.position);
+                }
+            }
+            else
+            {
+                actionQueue = null;
+            }
+        }
     }
 }
-
-
