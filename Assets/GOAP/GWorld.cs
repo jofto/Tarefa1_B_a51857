a4c62482e590d32726c80cbe9abca7a53049dd67
jofto <slammerex03@gmail.com>
2023-11-25 00:47:53 +0000

    3. Matching Agents with Agents

--- Assets/GOAP/GWorld.cs
+++ Assets/GOAP/GWorld.cs
@@ -1,23 +1,70 @@
-﻿public sealed class GWorld {
+﻿using System.Collections.Generic;
+using UnityEngine;
 
+public sealed class GWorld 
+{
     private static readonly GWorld instance = new GWorld();
     private static WorldStates world;
+    private static Queue<GameObject> patients;
+    private static Queue<GameObject> cubicles;
 
-    static GWorld() {
-
+    static GWorld() 
+    {
         world = new WorldStates();
+        patients = new Queue<GameObject>();
+        cubicles = new Queue<GameObject>();
+        GameObject[] cubes = GameObject.FindGameObjectsWithTag("Cubicle");
+        foreach (GameObject c in cubes) 
+        {
+
+            cubicles.Enqueue(c);
+        }
+
+        // Inform the state
+        if (cubes.Length > 0) 
+        {
+            world.ModifyState("FreeCubicle", cubes.Length);
+        }
+
+        Time.timeScale = 5.0f;
     }
 
-    private GWorld() {
+    private GWorld() 
+    {
 
     }
 
-    public static GWorld Instance {
+    public void AddPatient(GameObject p) 
+    {
+        patients.Enqueue(p);
+    }
+
+    public GameObject RemovePatient() 
+    {
+
+        if (patients.Count == 0) return null;
+        return patients.Dequeue();
+    }
+
+    public void AddCubicle(GameObject p) 
+    {
+        cubicles.Enqueue(p);
+    }
+
+    public GameObject RemoveCubicle() 
+    {
+        if (cubicles.Count == 0) return null;
+        return cubicles.Dequeue();
+    }
+
+    public static GWorld Instance 
+    {
 
         get { return instance; }
     }
 
-    public WorldStates GetWorld() {
+    public WorldStates GetWorld() 
+    {
 
         return world;
     }
